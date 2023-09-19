1c3cd510558cbfb476e2ac92e2a93c44597aba40
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2023-09-18 17:50:30 -0700

    Fixed opacity transition positioning styles, centered temp pictures for Projects and Resume

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import kitten from '../assets/img/kittenTestPic.PNG';
+import styles from './Projects.module.css';
 
 function DisplayProjects() {
     return (
+        <div className={styles.tempCenterDiv}>
+            <div className={styles.tempContainer}>
         <img src={kitten} alt="Kitten" />
-    )
+            </div>
+        </div>
+        )
     
 }
 
--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import kitten from '../assets/img/kittenTestPic.PNG';
+import styles from './Resume.module.css';
 
 function Resume() {
     return (
+        <div className={styles.tempCenterDiv}>
         <img src={kitten} alt="kitten" />
+        </div>
     );
 }
 
