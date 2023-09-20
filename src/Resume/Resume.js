f7d35f0a213a967314ce00d9f9ce3825ce932c7a
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2023-09-20 15:05:35 -0700

    PDF download link test

--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import kitten from '../assets/img/kittenTestPic.PNG';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
+
+
 import styles from './Resume.module.css';
 
 function Resume() {
     return (
-        <div className={styles.tempCenterDiv}>
-        <img src={kitten} alt="kitten" />
+    <div className={styles.primaryContainer}>
+        <div className={styles.iconContainer}>
+        <a href="https://www.linkedin.com/in/cambria-sullivan-ab3647a6/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" className={styles.iconLinkedin} />
+        </a>
+        <a href="../../public/Cambria_Sullivan_Resume" download>
+            <FontAwesomeIcon icon={faFileDownload} size="2x" className={styles.iconDownload} />
+        </a>
+        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="2x" className={styles.iconGithub} />
+        </a>
         </div>
+    </div>
     );
 }
 
