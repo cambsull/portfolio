6a2b24c2a0d3e7fe20f9048cdfb5dc1837181ab3
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2023-12-07 16:52:58 -0800

    Removed development build text

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
     return (
        <>
         <Header onButtonClick={handleButtonClick} />
-        <div className="developmentBuild"><p>Development Build: 0.8</p></div>
         <TransitionGroup>
             <CSSTransition key={displayComponent} classNames="opacityEffect" timeout={300}>
                 <>
